ce3f5f610146e18d9cd86e3e35121341f160edaa
OchoaHub <47892571+OchoaHub@users.noreply.github.com>
2019-05-31 12:34:40 -0500

    Add files via upload

--- seleccion.php
+++ seleccion.php
@@ -1,3 +1,5 @@
+<html>
+<body>
 <?php
   // Datos de la tabla
   $servername = "localhost";
@@ -16,17 +18,36 @@
   $sql = "SELECT id, name, email FROM ingenieros";
   $result = $conn->query($sql);
 
-  if($result->num_rows > 0)
+  echo "<table border='1'>
+  <tr>
+  <th>ID</th>
+  <th>Name</th>
+  <th>Email</th>
+  </tr>";
+
+  while($row = mysqli_fetch_array($result))
   {
-    // Imprimes lo de cada columna
-    while($row = $result->fetch_assoc())
-    {
-      echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
-    }
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "</tr>";
   }
-  else
-  {
-    echo "0 results";
-  }
+  echo "</table>";
+
   $conn->close();
+
+  // if($result->num_rows > 0)
+  // {
+  //   // Imprimes lo de cada columna
+  //   while($row = $result->fetch_assoc())
+  //   {
+  //     echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+  //   }
+  // }
+  // else
+  // {
+  //   echo "0 results";
+  // }
+  // $conn->close();
   ?>
